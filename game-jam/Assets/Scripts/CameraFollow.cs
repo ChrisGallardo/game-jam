57a065da12809b5f024bb4a836ce2172250fb57d
Chris Gallardo <chrisgallardo@live.com>
2021-02-15 12:02:06 -0800

    Camera Follow script

---
+++ game-jam/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Transform player;
+
+    private void LateUpdate()
+    {
+        transform.position = player.position + offset;
+    }
+}
